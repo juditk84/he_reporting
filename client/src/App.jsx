f032241b57d0a960e1b7a82ffe7e69ac611a5b2f
Judit Calero <judit.calero84@gmail.com>
2024-01-16 16:48:06 +0100

    started a test tabs layout, looking good. Goona feed it with data and see what happens

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/home';
+import Project from './pages/project';
+import Expenses from './pages/expenses';
+import Personnel from './pages/personnel';
+import Landing from './pages/landing';
+import NavBar from './components/navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}>
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="/personnel" element={<Personnel />} />
+        </Route>
+        
       </Routes>
     </div>
 
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
+import { Tab, Tabs } from 'react-bootstrap'
+import { useState } from 'react'
 
 export default function home() {
+
+  const [key, setKey] = useState();
+  const navigate = useNavigate();
+
   return (
     <div>
-        <h1>GONFLANT</h1>
-    
-    
-    
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => navigate(`/${k}`)}
+        className="mb-3">
+
+        <Tab eventKey="project" title="Project Summary">
+          <Outlet />
+        </Tab>
+        <Tab eventKey="expenses" title="Expenses">
+          <Outlet />
+        </Tab>
+        <Tab eventKey="personnel" title="Personnel">
+          <Outlet />
+        </Tab>
+      </Tabs>
     </div>
   )
 }
